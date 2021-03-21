272f701c14039ddb7730f71d34042e08b09a0f94
Milind452 <mohapatra.milind@gamil.com>
2021-03-21 16:59:21 +0530

    Complete Task 2

--- P0/Task2.py
+++ P0/Task2.py
@@ -3,11 +3,11 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
-with open('texts.csv', 'r') as f:
+with open('P0/texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('P0/calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -19,4 +19,14 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+m = -1
+mNumber = ''
+for call in calls:
+    number = call[0]
+    duration = int(call[3])
+    if duration > m:
+        m = duration
+        mNumber = number
+
+print("{} spent the longest time, {} seconds, on the phone during September 2016".format(mNumber, m))
 
